152f11efb1e527340203b547779cabeca3fb5b86
broniu <broniekf@gmail.com>
2024-04-24 17:41:24 +0200

    fix black text

---
+++ src/CinemaBooking.Web/Extensions/NotificationServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Radzen;
+
+namespace CinemaBooking.Web.Extensions;
+
+public static class NotificationServiceExtensions
+{
+    public static void NotifyError(this NotificationService source, string message) =>
+        source.Notify(new NotificationMessage { Severity = NotificationSeverity.Error, Detail = message});
+
+    public static void NotifySuccess(this NotificationService source, string message) =>
+        source.Notify(new NotificationMessage { Severity = NotificationSeverity.Success, Detail = message });
+}
