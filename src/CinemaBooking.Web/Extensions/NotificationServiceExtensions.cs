de25f870d751f71f5c870e3cca8b5b32c65cfa56
broniu <broniekf@gmail.com>
2024-04-25 12:38:05 +0200

    Fix notifications position

--- src/CinemaBooking.Web/Extensions/NotificationServiceExtensions.cs
+++ src/CinemaBooking.Web/Extensions/NotificationServiceExtensions.cs
@@ -5,8 +5,18 @@ namespace CinemaBooking.Web.Extensions;
 public static class NotificationServiceExtensions
 {
     public static void NotifyError(this NotificationService source, string message) =>
-        source.Notify(new NotificationMessage { Severity = NotificationSeverity.Error, Detail = message});
+        source.Notify(
+            new NotificationMessage
+            {
+                Severity = NotificationSeverity.Error,
+                Detail = message,
+            });
 
     public static void NotifySuccess(this NotificationService source, string message) =>
-        source.Notify(new NotificationMessage { Severity = NotificationSeverity.Success, Detail = message });
+        source.Notify(
+            new NotificationMessage
+            {
+                Severity = NotificationSeverity.Success,
+                Detail = message,
+            });
 }
