b9c2120c182248fd11b0a15662a6e6937651148d
broniu <broniekf@gmail.com>
2024-04-25 12:17:21 +0200

    Remake get hall view

--- src/CinemaBooking.Web/Services/HallService.cs
+++ src/CinemaBooking.Web/Services/HallService.cs
@@ -12,27 +12,36 @@ public class HallService(IDbContextFactory<CinemaDbContext> dbContextFactory, IL
     private readonly IDbContextFactory<CinemaDbContext> _dbContextFactory = dbContextFactory;
     private readonly ILogger<HallService> _logger = logger;
 
-    public async Task<Result<HallForView>> GetHallViewAsync(Guid hallId)
+    public async Task<Result<HallForView?>> GetHallViewAsync()
     {
-        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
-        var hall = await dbContext.Halls.FindAsync(hallId);
-        if (hall is null)
-        {
-            _logger.LogError("Hall with Id: {HallId} doesn't exists", hallId);
-            var error = new ArgumentException("Error occured while getting the halls");
-            return new Result<HallForView>(error);
-        }
-        var seatsByRow = await dbContext.Seats.Where(s => s.HallId == hallId).GroupBy(s => s.PositionY).ToListAsync();
-        var rowsForView = seatsByRow.Select(r => new SeatsRowForView([.. r.OrderBy(s => s.PositionX).Select(r => r.CreateForView())]));
-        return new HallForView(hall.Name, [.. rowsForView]);
-    }
-
-    public async Task<Result<Hall?>> GetHallAsync()
-    {
-        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
         var exceptionMessage = "Error occured when getting the hall";
         try
         {
+            await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+            var hall = await dbContext.Halls.SingleOrDefaultAsync(); // DB should contain only one hall
+            if (hall is null)
+            {
+                _logger.LogError("Halls contains no elements");
+                return new Result<HallForView?>(new Exception(exceptionMessage));
+            }
+            var seatsByRow = await dbContext.Seats.Where(s => s.HallId == hall.Id).GroupBy(s => s.PositionY).ToListAsync();
+            var rowsForView = seatsByRow.Select(r => new SeatsRowForView([.. r.OrderBy(s => s.PositionX).Select(r => r.CreateForView())]));
+            return new HallForView(hall.Name, [.. rowsForView]);
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.LogError(ex, "Halls contains more than one element");
+            return new Result<HallForView?>(new Exception(exceptionMessage));
+        }
+    }
+
+
+    public async Task<Result<Hall?>> GetHallAsync()
+    {
+        var exceptionMessage = "Error occured when getting the hall";
+        try
+        {
+            await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
             var hall = await dbContext.Halls.SingleOrDefaultAsync(); // DB should contain only one hall
             if (hall is null)
             {
