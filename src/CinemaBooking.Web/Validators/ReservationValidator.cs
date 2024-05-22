3606041576fcd3cb95ac3792ebf99c3dfaa71def
broniu <broniekf@gmail.com>
2024-05-22 13:57:34 +0200

    Add validators translation

--- src/CinemaBooking.Web/Validators/ReservationValidator.cs
+++ src/CinemaBooking.Web/Validators/ReservationValidator.cs
@@ -1,16 +1,18 @@
-﻿using CinemaBooking.Web.Db.Entitites;
+﻿using CinemaBooking.Web.Components.Pages.Screenings;
+using CinemaBooking.Web.Db.Entitites;
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 
 namespace CinemaBooking.Web.Validators;
 
 public class ReservationValidator : AbstractValidator<Reservation>
 {
-    public ReservationValidator()
+    public ReservationValidator(IStringLocalizer<ScreeningsPage> localizer)
     {
         RuleFor(r => r.Id).NotEmpty();
-        RuleFor(r => r.Name).NotEmpty();
-        RuleFor(r => r.Surname).NotEmpty();
-        RuleFor(r => r.PhoneNumber).Matches(@"^\s*\+?\d+(?:\s\d+)*\s*");
+        RuleFor(r => r.Name).NotEmpty().WithMessage(localizer["NameNotEmpty"]);
+        RuleFor(r => r.Surname).NotEmpty().WithMessage(localizer["SurnameNotEmpty"]);
+        RuleFor(r => r.PhoneNumber).Matches(@"^\s*\+?\d+(?:\s\d+)*\s*").WithMessage(r => $"{r.PhoneNumber} {localizer["BadPhoneNumber"]}");
         RuleFor(r => r.ScreeningId).NotEmpty();
         RuleFor(r => r.SeatId).NotEmpty();
     }
